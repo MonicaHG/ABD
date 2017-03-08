797b6a4b47a4877e3868b0547d0b6e18d5599358
MonicaHG <monicahg_93@hotmail.com>
2017-03-08 15:35:58 +0100

    Login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login - Biblioteca</title>
+		<link rel="stylesheet" type="text/css" href="estilos.css"/>
+	</head>
+	<body>
+		<div class="contenido">
+			<div class="formulario">
+				<form action="formLogin.php" method="POST">
+					<p><h2><align = center> <font color= white> Inicio de sesión </font></h2></p>
+					<p><input type="text" name="usuario" placeholder= "Usuario" required > (*)</input> </p>
+					<p><input type="password" name="pass" placeholder= "Password" required> (*)</input></p>
+					<input type="submit" name="submit" value="Login">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
